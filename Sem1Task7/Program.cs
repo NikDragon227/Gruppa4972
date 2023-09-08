9370a6143fdef91121868294bdbaa94b26d79310
klyavin Nikita <nikita-driving@mail.ru>
2023-09-08 11:29:55 +0300

    доделал task 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 100 && num < 1000)
+{
+    int lastDigit = num % 10;
+
+    Console.WriteLine("Последняя цифра числа: " + lastDigit);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное.");
+}
