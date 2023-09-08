3d98c321d81cd9b07f5c89835c6a284fa2568b4b
klyavin Nikita <nikita-driving@mail.ru>
2023-09-08 11:14:48 +0300

    Выполнил ДЗ к 1 семинару

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (firstNumber > max)
+{
+    max = firstNumber;
+}
+
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+
+if(threeNumber > max)
+{
+    max = threeNumber;
+}
+
+Console.WriteLine(max);
