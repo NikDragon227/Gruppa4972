7e58d1ff151a5b7fbf403cc9bdb1b3b2c567b405
klyavin Nikita <nikita-driving@mail.ru>
2023-09-15 11:57:05 +0300

    Homework sem2 task 10, 13, 15

---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,38 @@
+﻿// Console.Write("Введите цифру, обозначающую день недели: ");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+// void CheckingTheDayOfTheWeek (int dayNumber) {
+//   if (dayNumber == 6 || dayNumber == 7) {
+//   Console.WriteLine("(этот день выходной) -> да");
+//   }
+//   else if (dayNumber < 1 || dayNumber > 7) {
+//     Console.WriteLine("это вообще не день недели");
+//   }
+//   else Console.WriteLine("(этот день не выходной) -> нет");
+// }
+
+// CheckingTheDayOfTheWeek(dayNumber);
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+var mount = new Dictionary<int, string>()
+{
+    [1] = "Понедельник",
+    [2] = "Вторник",
+    [3] = "Среда",
+    [4] = "Четверг",
+    [5] = "Пятница",
+    [6] = "Суббота",
+    [7] = "Воскресенье",
+};
+
+if(dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Этот день выходной.");
+}
+else
+{
+    Console.WriteLine("Этот день не явлется выходным.");
+}
